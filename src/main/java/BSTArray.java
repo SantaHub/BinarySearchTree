c179353eb6c7de28387c7e6284f07a4122c44b22
Austin <ap84321N@pace.edu>
2021-12-18 15:53:22 -0500

    inorder and preorder

--- src/main/java/BSTArray.java
+++ src/main/java/BSTArray.java
@@ -3,6 +3,24 @@ public class BSTArray {
     int[] bstArray = new int[MAX_SIZE];
     int size = 0;
 
+    public static void main(String args[]){
+        BSTArray bstArray = new BSTArray();
+        bstArray.insert(4);
+        bstArray.insert(2);
+        bstArray.insert(3);
+        bstArray.insert(7);
+        bstArray.insert(4);
+        bstArray.insert(2);
+        bstArray.insert(3);
+        bstArray.insert(7);
+        for (int i : (bstArray.getBstArray())) {
+            System.out.print(i);
+        }
+
+        System.out.println("\n Printing Inorder");
+        bstArray.inOrderView(bstArray.getBstArray(), 0);
+    }
+
     public void insert(int data){
         if (size == MAX_SIZE) {
             System.out.println("Reached insertion limit. Call Austin.");
@@ -22,16 +40,38 @@ public class BSTArray {
         size = 0;
     }
 
-    public void inOrderView(){
-        // Left Root Right
+    public void postOrderView(int[] bstArraySegment, int rootIndex){
+        // Left Right Root
+        if(rootIndex == size) {
+            return;
+        } else if(bstArraySegment[rootIndex] < bstArraySegment[rootIndex+1]){ // Left
+            System.out.print(bstArraySegment[rootIndex]);
+            postOrderView(bstArraySegment, rootIndex+1);
+        } else if(bstArraySegment[rootIndex] >= bstArraySegment[rootIndex+1] ) { // Right
+            postOrderView(bstArraySegment, rootIndex+1);
+            System.out.print(bstArraySegment[rootIndex]);
+        }
     }
 
-    private int getInsertIndex(int data){
-        int index = 0;
-        for(int elem : bstArray) {
-            if(elem)
+
+    public void inOrderView(int[] bstArraySegment, int rootIndex){
+        // Left Root Right
+        if(rootIndex == size) {
+            return;
         }
-        return index;
+        if (bstArraySegment[rootIndex] > bstArraySegment[rootIndex +1 ]) { // handling left side
+            inOrderView(bstArraySegment, rootIndex+1);
+            System.out.print(bstArraySegment[rootIndex]);
+
+        } else if(bstArraySegment[rootIndex] <= bstArraySegment[rootIndex + 1]) { // handling right side
+            System.out.print(bstArraySegment[rootIndex]);
+            inOrderView(bstArraySegment, rootIndex+1);
+        }
     }
+
+    public int[] getBstArray(){
+        return bstArray;
+    }
+
 }
 
